a63659e255e4e299f876954a191b2e577bed35e8
maegsul <fahrikucukali@gmail.com>
2022-10-21 19:57:07 +0200

    incomplete code

--- scripts/STR_regression.R
+++ scripts/STR_regression.R
@@ -3,23 +3,24 @@
 
 # For now in R, to be converted into Rust
 
-# TODOS TO PREVENT USER STUPIDITY
-# TODO: check if mode is only SUM MIN or MAX, if not throw early error
+# TODOS TO PREVENT USER STUPIDITY - FK: language! :D
+# TODO: check if size_select is only MEAN, MIN or MAX, if not throw early error
 # TODO: check that if users supply a begin they also supply an end argument
 # TODO: either support running the whole file, or check that users at least supply one of chr, chr-begin-end or bed
-# TODO: add a progress bar :-D
+# TODO: add a progress bar :-D - FK: done! Using https://github.com/SciViews/svMisc .
 
 suppressPackageStartupMessages(library(data.table))
 suppressPackageStartupMessages(library(dplyr))
-suppressPackageStartupMessages(library(argparser, quietly = TRUE))
+suppressPackageStartupMessages(library(argparser))
+suppressPackageStartupMessages(library(svMisc))
 
 assoc_binary <- function(arg, calls_file, phenotype, no_cols, covariates) {
     binaryOrder <- gsub(",", " ", arg$binaryOrder)
     binaryOrder_prepared <- unlist(strsplit(binaryOrder, split = " "))
-    calls_file_selected <- as.data.table(calls_file[calls_file[[phenotype]] == c(binaryOrder_prepared), ])
+    calls_file_selected <- as.data.table(calls_file[calls_file[[phenotype]] %in% c(binaryOrder_prepared), ])
     calls_file_selected[[phenotype]] <- factor(calls_file_selected[[phenotype]], c(binaryOrder_prepared))
-    results_calls_file_selected <- as.data.frame(matrix(0, 1, 11))
-    colnames(results_calls_file_selected) <- c("VariantID", "OR", "OR_L95", "OR_U95", "OR_stdErr", "Pvalue", "N", "AvgSize", "Group1_AvgSize", "Group2_AvgSize", "model")
+    results_calls_file_selected <- as.data.frame(matrix(0, 1, 13))
+    colnames(results_calls_file_selected) <- c("VariantID", "OR", "OR_L95", "OR_U95", "OR_stdErr", "Pvalue", "N", "AvgSize", "Group1_AvgSize", "Group2_AvgSize", "Group2_Group1_absAvgSizeDiff", "Group2_Group1_OR_for_absAvgSizeDiff", "model")
     for (i in seq(no_cols, ncol(calls_file_selected), 1)) {
         VariantToBeTested <- as.character(colnames(calls_file_selected)[i])
         selectedtable <- as.data.table(cbind(as.character(calls_file_selected[[phenotype]]), as.numeric(calls_file_selected[[VariantToBeTested]])))
@@ -29,6 +30,7 @@ assoc_binary <- function(arg, calls_file, phenotype, no_cols, covariates) {
         AvgSize <- round(mean(as.numeric(selectedtable[[VariantToBeTested]]), na.rm = TRUE), digits = 3)
         Group2_AvgSize <- round(mean(as.numeric(group2[[VariantToBeTested]]), na.rm = TRUE), digits = 3)
         Group1_AvgSize <- round(mean(as.numeric(group1[[VariantToBeTested]]), na.rm = TRUE), digits = 3)
+        Group2_Group1_absAvgSizeDiff <- round(abs(Group2_AvgSize - Group1_AvgSize), digits = 3)
         if (!is.na(covariates)) {
             covlist <- gsub(",", " ", covariates)
             covlist_prepared <- unlist(strsplit(covlist, split = " "))
@@ -45,20 +47,22 @@ assoc_binary <- function(arg, calls_file, phenotype, no_cols, covariates) {
         OR_stdErr <- round(as.numeric(coef(summary(glm_result))[, "Std. Error"]), digits = 3)
         Pvalue <- as.numeric(coef(summary(glm_result))[, "Pr(>|z|)"])
         N <- nobs(glm_result)
+        Group2_Group1_OR_for_absAvgSizeDiff <- round((exp(Group2_Group1_absAvgSizeDiff*log(OR))), digits=3)
         model <- as.character(glm_result$formula)[1]
-        tabular_result <- as.data.frame(cbind(Predictors, OR, OR_L95, OR_U95, OR_stdErr, Pvalue, N, AvgSize, Group1_AvgSize, Group2_AvgSize, model))
+        tabular_result <- as.data.frame(cbind(Predictors, OR, OR_L95, OR_U95, OR_stdErr, Pvalue, N, AvgSize, Group1_AvgSize, Group2_AvgSize, Group2_Group1_absAvgSizeDiff, Group2_Group1_OR_for_absAvgSizeDiff, model))
         tabular_result <- subset(tabular_result, Predictors == VariantID)
         colnames(tabular_result)[1] <- "VariantID"
         results_calls_file_selected <- rbind.data.frame(results_calls_file_selected, tabular_result)
+        progress(i, max.value = ncol(calls_file_selected), init = TRUE, progress.bar = FALSE, console = TRUE, gui = FALSE)
     }
-    results_calls_file_selected <- results_calls_file_selected[-1, ]
-    sorted_results_calls_file_selected <- results_calls_file_selected[order(results_calls_file_selected$Pvalue), ]
+    results_calls_file_selected <- as.data.table(results_calls_file_selected[-1, ])
+    sorted_results_calls_file_selected <- results_calls_file_selected[order(as.numeric(results_calls_file_selected$Pvalue)), ]
     write.table(sorted_results_calls_file_selected, arg$out, sep = "\t", col.names = TRUE, quote = F, row.names = F)
 }
 
 assoc_continuous <- function(arg, calls_file, phenotype, no_cols, covariates) {
-    results_calls_file <- as.data.frame(matrix(0, 1, 11))
-    colnames(results_calls_file) <- c("VariantID", "Beta", "Beta_L95", "Beta_U95", "Beta_stdErr", "Pvalue", "N", "AvgSize", "MinSize", "MaxSize", "model")
+    results_calls_file <- as.data.frame(matrix(0, 1, 13))
+    colnames(results_calls_file) <- c("VariantID", "Beta", "Beta_L95", "Beta_U95", "Beta_stdErr", "Pvalue", "N", "AvgSize", "MinSize", "MaxSize", "Max_Min_absSizeDiff", "Max_Min_Beta_for_absSizeDiff", "model")
     for (i in seq(no_cols, ncol(calls_file), 1)) {
         VariantToBeTested <- as.character(colnames(calls_file)[i])
         selectedtable <- as.data.table(cbind(as.character(calls_file[[phenotype]]), as.numeric(calls_file[[VariantToBeTested]])))
@@ -66,6 +70,7 @@ assoc_continuous <- function(arg, calls_file, phenotype, no_cols, covariates) {
         AvgSize <- round(mean(as.numeric(selectedtable[[VariantToBeTested]]), na.rm = TRUE), digits = 3)
         MaxSize <- round(max(as.numeric(selectedtable[[VariantToBeTested]]), na.rm = TRUE), digits = 3)
         MinSize <- round(min(as.numeric(selectedtable[[VariantToBeTested]]), na.rm = TRUE), digits = 3)
+        Max_Min_absSizeDiff <- round(abs(max(as.numeric(selectedtable[[VariantToBeTested]]), na.rm = TRUE) - min(as.numeric(selectedtable[[VariantToBeTested]]), na.rm = TRUE)), digits = 3)
         if (!is.na(covariates)) {
             covlist <- gsub(",", " ", covariates)
             covlist_prepared <- unlist(strsplit(covlist, split = " "))
@@ -82,20 +87,22 @@ assoc_continuous <- function(arg, calls_file, phenotype, no_cols, covariates) {
         Beta_stdErr <- round(as.numeric(coef(summary(glm_result))[, "Std. Error"]), digits = 3)
         Pvalue <- as.numeric(coef(summary(glm_result))[, "Pr(>|t|)"])
         N <- nobs(glm_result)
+        Max_Min_Beta_for_absSizeDiff <- round((Max_Min_absSizeDiff*Beta), digits=3)
         model <- as.character(glm_result$formula)[1]
-        tabular_result <- as.data.frame(cbind(Predictors, Beta, Beta_L95, Beta_U95, Beta_stdErr, Pvalue, N, AvgSize, MinSize, MaxSize, model))
+        tabular_result <- as.data.frame(cbind(Predictors, Beta, Beta_L95, Beta_U95, Beta_stdErr, Pvalue, N, AvgSize, MinSize, MaxSize, Max_Min_absSizeDiff, Max_Min_Beta_for_absSizeDiff, model))
         tabular_result <- subset(tabular_result, Predictors == VariantID)
         colnames(tabular_result)[1] <- "VariantID"
         results_calls_file <- rbind.data.frame(results_calls_file, tabular_result)
+        progress(i, max.value = ncol(calls_file_selected), init = (value == 1), progress.bar = TRUE, consolute = TRUE, gui = FALSE)
     }
     results_calls_file <- results_calls_file[-1, ]
-    sorted_results_calls_file <- results_calls_file[order(results_calls_file$Pvalue), ]
+    sorted_results_calls_file <- results_calls_file[order(as.numeric(results_calls_file$Pvalue)), ]
     write.table(sorted_results_calls_file, arg$out, sep = "\t", col.names = TRUE, quote = F, row.names = F)
 }
 
-read_calls <- function(input, chrom) {
+read_calls <- function(input, chr) {
     calls_file <- fread(input, header = TRUE)
-    calls_file <- subset(calls_file, chrom == chrom)
+    calls_file <- subset(calls_file, chrom == chr)
     strnames <- paste(calls_file$chrom, calls_file$begin, calls_file$end, sep = "_")
     rest <- calls_file[, -c(1:3)]
     col_index <- seq(1:ncol(rest))
@@ -104,12 +111,11 @@ read_calls <- function(input, chrom) {
     colnames(inqH1) <- gsub(".[^.]+$", "", colnames(inqH1))
     colnames(inqH2) <- gsub(".[^.]+$", "", colnames(inqH2))
     return(list("H1" = inqH1, "H2" = inqH2, "strnames" = strnames))
-    rm(calls_file)
 }
 
 read_calls_chr_begin_end <- function(input, chr, chr_begin, chr_end) {
     calls_file <- fread(input, header = TRUE)
-    calls_file <- subset(calls_file, ((chr == chr) & (chr_begin >= chr_begin) & (chr_end <= chr_end)))
+    calls_file <- subset(calls_file, ((chrom == chr) & (begin >= chr_begin) & (end <= chr_end)))
     strnames <- paste(calls_file$chrom, calls_file$begin, calls_file$end, sep = "_")
     rest <- calls_file[, -c(1:3)]
     col_index <- seq(1:ncol(rest))
@@ -118,7 +124,6 @@ read_calls_chr_begin_end <- function(input, chr, chr_begin, chr_end) {
     colnames(inqH1) <- gsub(".[^.]+$", "", colnames(inqH1))
     colnames(inqH2) <- gsub(".[^.]+$", "", colnames(inqH2))
     return(list("H1" = inqH1, "H2" = inqH2, "strnames" = strnames))
-    rm(calls_file)
 }
 
 read_calls_bed <- function(input, bed) {
@@ -128,7 +133,7 @@ read_calls_bed <- function(input, bed) {
     colnames(calls_file)[2] <- "start"
     intersecttable <- as.data.table(bed_intersect(calls_file, bedfile, suffix = c("", ".y")))
     intersecttable <- intersecttable[, 1:(length(intersecttable) - 3)]
-    intersecttable <- subset(intersecttable, !is.na(chrom)) ## WDC here is something wrong - there is no chrom
+    intersecttable <- subset(intersecttable, !is.na(chrom)) ## WDC here is something wrong - there is no chrom ## FK - no it is fine, we can keep it is. It is just to make sure that there are no "NA" entries in the datatable, based on a chrom column != NA condition (I did it because I've seen it sometimes after bed_intersect). (feel free to remove this comment)
     colnames(intersecttable)[2] <- "begin"
     intersect_strnames <- paste(intersecttable$chrom, intersecttable$begin, intersecttable$end, sep = "_")
     rest <- intersecttable[, -c(1:3)]
@@ -138,9 +143,6 @@ read_calls_bed <- function(input, bed) {
     colnames(inqH1) <- gsub(".[^.]+$", "", colnames(inqH1))
     colnames(inqH2) <- gsub(".[^.]+$", "", colnames(inqH2))
     return(list("H1" = inqH1, "H2" = inqH2, "strnames" = intersect_strnames))
-    rm(calls_file)
-    rm(intersecttable)
-    rm(bedfile)
 }
 
 prepare_phenotype <- function(phenofile, phenotype, sample_list) {
@@ -153,13 +155,13 @@ prepare_phenotype <- function(phenofile, phenotype, sample_list) {
     ))
 }
 
-prepare_calls <- function(calls, sample_list_wPheno, mode, missing_cutoff) {
-    if (mode == "SUM") {
-        calls2 <- calls$H1 + calls$H2
-    } else if (mode == "MAX") {
-        calls2 <- pmax(calls$H1, calls$H2)
-    } else if (mode == "MIN") {
-        calls2 <- pmin(calls$H1, calls$H2)
+prepare_calls <- function(calls, sample_list_wPheno, size_select, missing_cutoff) {
+    if (size_select == "MEAN") {
+        calls2 <- (pmax(calls$H1, calls$H2,na.rm=TRUE) + pmin(inqH1[,2],inqH2[,2],na.rm=TRUE)) / 2
+    } else if (size_select == "MAX") {
+        calls2 <- pmax(calls$H1, calls$H2, na.rm = TRUE)
+    } else if (size_select == "MIN") {
+        calls2 <- pmin(calls$H1, calls$H2, na.rm = TRUE)
     }
     calls_file <- transpose(calls2)
     colnames(calls_file) <- calls$strnames
@@ -171,27 +173,32 @@ prepare_calls <- function(calls, sample_list_wPheno, mode, missing_cutoff) {
 }
 
 parse_arguments <- function() {
-    p <- arg_parser("Run association testing for STRs with different modes and options. Version 1.3, October 20, 2022")
-    p <- add_argument(p, "--input", help = "Input STR file with a header, first 3 columns are chrom, begin, end, and rest are sample ids with inqH1 & inqH2 STR lengths", type = "character", nargs = 1)
+    p <- arg_parser("Run association testing for STRs with different modes and options. Version 1.4, October 21, 2022")
+    p <- add_argument(p, "--input", help = "Input STR file with a header, first 3 columns are chrom, begin, end, and rest are sample ids with inqH1 & inqH2 STR lengths", type = "character", nargs = 1, required = TRUE)
     p <- add_argument(p, "--phenocovar", help = "Phenotype and covariate file with header, first column is sample_id", type = "character", nargs = 1)
     p <- add_argument(p, "--covnames", help = "Covariate names you want to use (optional), separated by comma", type = "character", nargs = "*")
     p <- add_argument(p, "--phenotype", help = "Column name of your phenotype of interest variable in the --phenocovar file", type = "character", nargs = 1)
     p <- add_argument(p, "--out", help = "Output file name", type = "character", nargs = 1)
-    p <- add_argument(p, "--mode", help = "Select a mode name from following: SUM, MAX, MIN", type = "character", nargs = 1)
-    p <- add_argument(p, "--missing_cutoff", help = "Defines the call rate cutoff for variants, default is 0.80 meaning that keeping all variants present in at least 80% of individuals", type = "numeric", default = "0.80")
+    p <- add_argument(p, "--size_select", help = "Choose a size_select name from following: MEAN, MAX, MIN; meaning H1+H2 alleles divided by two, maximum of two, or minimum of two. Missing alleles are not considered.", type = "character", nargs = 1)
+    p <- add_argument(p, "--missing_cutoff", help = "Defines the call rate cutoff for variants, default is 0.80 meaning that keeping all variants present in at least 80% of individuals. Might mean different things in each of the MEAN, MAX, MIN modes, use carefully.", type = "numeric", default = "0.80")
     p <- add_argument(p, "--outcometype", help = "Select a outcome variable type: binary or continuous", type = "character", nargs = 1)
     p <- add_argument(p, "--binaryOrder", help = "Give the binary phenotype order, comma separated, e.g. Control, Patient will code Control as 0/Group1 and Patient as 1/Group2. This will also be used to further filter your data to only two groups (if you had more than >2 groups for your categorical data)", type = "character", nargs = "*")
-    p <- add_argument(p, "--chr", help = "Indicate chromosome number to be analyzed (with chr prefix). Optional if bed file is provided.", type = "character", nargs = "*")
-    p <- add_argument(p, "--chr_begin", help = "Define a begin position (inclusive) for a region of interest (optional, and should be combined with --chr_end)", type = "integer", nargs = "*")
-    p <- add_argument(p, "--chr_end", help = "Define a end position (inclusive) for a region of interest (optional, and should be combined with --chr_begin)", type = "integer", nargs = "*")
-    p <- add_argument(p, "--bed", help = "A bed file (without a header) with three columns: chromosome (with chr prefix), begin, and end positions for region(s) of interest (optional). valr Rpackage is required - can be installed with mamba install r-valr on conda environment", type = "character", nargs = "*")
-    Version <- "Run association testing for STRs with different modes and options. Version 1.3, October 20, 2022"
+    p <- add_argument(p, "--run", help = "Run mode, select among: full (entire file will be analyzed), chromosome (chromosome indicated by --chr will be analyzed), chr_interval (intervals between chr:begin:end defined by --chr --begin --end will be analyzed), bed_interval (an intersection with a bed file defined by --bed will be analyzed), single_variant (a single variant defined by --single_variant will be analyzed; can be combined with --expandedAllele)", type = "character", nargs = 1)
+    p <- add_argument(p, "--chr", help = "Indicate chromosome number to be analyzed (with chr prefix). Optional if bed file is provided.", type = "character", nargs = "?")
+    p <- add_argument(p, "--chr_begin", help = "Define a begin position (inclusive) for a region of interest (optional, and should be combined with --chr_end)", type = "integer", nargs = "?")
+    p <- add_argument(p, "--chr_end", help = "Define a end position (inclusive) for a region of interest (optional, and should be combined with --chr_begin)", type = "integer", nargs = "?")
+    p <- add_argument(p, "--bed", help = "A bed file (without a header) with three columns: chromosome (with chr prefix), begin, and end positions for region(s) of interest (optional). valr Rpackage is required - can be installed with mamba install r-valr on conda environment", type = "character", nargs = "?")
+    p <- add_argument(p, "--expandedAllele", help = "A cut-off integer number to define 2 groups with and without expanded allele", type = "integer", nargs = "?")
+    Version <- "Version 1.4, October 21, 2022"
     print(Version)
 
     return(parse_args(p))
 }
 
 arg <- parse_arguments()
+
+#checks
+
 
 # calls is a list with H1, H2 and strnames attributes
 if ((!is.na(arg$chr) && !is.na(arg$chr_begin) && !is.na(arg$chr_end))) {
@@ -200,7 +207,7 @@ if ((!is.na(arg$chr) && !is.na(arg$chr_begin) && !is.na(arg$chr_end))) {
     suppressPackageStartupMessages(library(valr))
     calls <- read_calls_bed(input = arg$input, bed = arg$bed)
 } else if (!is.na(arg$chr)) {
-    calls <- read_calls(input = arg$input, chrom = arg$chr)
+    calls <- read_calls(input = arg$input, chr = arg$chr)
 }
 
 # pheno_info is a list with sample_list, phenotype and no_cols attributes
@@ -213,7 +220,7 @@ pheno_info <- prepare_phenotype(
 calls_file <- prepare_calls(
     calls = calls,
     sample_list_wPheno = pheno_info$sample_list,
-    mode = arg$mode,
+    size_select = arg$size_select,
     missing_cutoff = arg$missing_cutoff
 )
 
